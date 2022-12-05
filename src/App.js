fb3f8e2c58fd5f2229fb32ba6931815b34c1027a
seongeon <dbtjsrjs8282@naver.com>
2022-12-05 22:47:12 +0900

    날씨 api 가져오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+
+      console.log("현재위치 ", lat, lon);
+      getWeatherByCurrentLocation(lat, lon);
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9174a3dd9f480af2752aa0eb0c28ab53`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data", data);
+  };
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  return <div className="App">dd</div>;
 }
 
 export default App;
